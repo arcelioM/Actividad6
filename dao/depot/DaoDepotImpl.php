c3c3f9901b426abfc269dac12fd0d766872da56e
arcelioM <arceliomonte15@gmail.com>
2022-11-11 16:09:17 -0500

    Funcionalidad de busqueda por ID para depot realizada

--- dao/depot/DaoDepotImpl.php
+++ dao/depot/DaoDepotImpl.php
@@ -1,41 +1,63 @@
 <?php
 
 namespace dao\depot;
+
 use dao\connection\IConnection;
 use PDOException;
 use util\Log;
 use PDO;
 
-class DaoDepotImpl implements IDaoDepot{
+class DaoDepotImpl implements IDaoDepot
+{
 
     private IConnection $connection;
 
     public function __construct(IConnection $connection)
     {
-        $this->connection=$connection;
+        $this->connection = $connection;
     }
-    public function getAll(){
-        try{
-            Log::write("INICIO DE CONSULTA DE BUSQUEDA","SELECT");
-            $query="SELECT ID_DEPOT,branchName,location,idStatus FROM depot ORDER BY ID_DEPOT DESC";
+    public function getAll()
+    {
+        try {
+            Log::write("INICIO DE CONSULTA DE BUSQUEDA", "SELECT");
+            $query = "SELECT ID_DEPOT,branchName,location,idStatus FROM depot ORDER BY ID_DEPOT DESC";
             $execute = $this->connection->getConnection()->prepare($query);
             $execute->execute();
             $result = $execute->fetchAll(PDO::FETCH_ASSOC);
             Log::write("CONSULTA EXITOSA", "INFO");
             return $result;
-        }catch(PDOException $e){
+        } catch (PDOException $e) {
             Log::write("dao\depot\DaoDepotImpl", "ERROR");
-			Log::write("ARCHIVO: " . $e->getFile() . " | lINEA DE ERROR: " . $e->getLine() . " | MENSAJE" . $e->getMessage(), "ERROR");
-			return array();
+            Log::write("ARCHIVO: " . $e->getFile() . " | lINEA DE ERROR: " . $e->getLine() . " | MENSAJE" . $e->getMessage(), "ERROR");
+            return array();
         }
     }
-    public function getByID($id){
+    public function getByID($id)
+    {
 
+        try {
+            Log::write("INICIO DE CONSULTA POR ID", "SELECT");
+            $query = "SELECT ID_DEPOT,branchName,location,idStatus FROM depot WHERE ID_DEPOT=?";
+            $args=array($id);
+
+            $execute = $this->connection->getConnection()->prepare($query);
+            $execute->execute($args);
+
+            $result = $execute->fetchAll(PDO::FETCH_ASSOC);
+            Log::write("CONSULTA REALIZADA CON EXITO","INFO");
+            return $result;
+        } catch (PDOException $e) {
+            Log::write("dao\depot\DaoDepotImpl", "ERROR");
+            Log::write("ARCHIVO: " . $e->getFile() . " | lINEA DE ERROR: " . $e->getLine() . " | MENSAJE" . $e->getMessage(), "ERROR");
+            return array();
+        }
     }
-    public function save($entidad):int{
+    public function save($entidad): int
+    {
         return 0;
     }
-    public function update($entidad):int{
+    public function update($entidad): int
+    {
         return 0;
     }
 }
