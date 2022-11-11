0e80dcf0c8c89202de0e1e360f9ec68bcaf84ca7
arcelioM <arceliomonte15@gmail.com>
2022-11-11 16:03:34 -0500

    Se implemento funcion de consulta de todos los registros para Depot

--- dao/depot/DaoDepotImpl.php
+++ dao/depot/DaoDepotImpl.php
@@ -1,11 +1,33 @@
 <?php
 
 namespace dao\depot;
+use dao\connection\IConnection;
+use PDOException;
+use util\Log;
+use PDO;
 
 class DaoDepotImpl implements IDaoDepot{
 
-    public function getAll(){
+    private IConnection $connection;
 
+    public function __construct(IConnection $connection)
+    {
+        $this->connection=$connection;
+    }
+    public function getAll(){
+        try{
+            Log::write("INICIO DE CONSULTA DE BUSQUEDA","SELECT");
+            $query="SELECT ID_DEPOT,branchName,location,idStatus FROM depot ORDER BY ID_DEPOT DESC";
+            $execute = $this->connection->getConnection()->prepare($query);
+            $execute->execute();
+            $result = $execute->fetchAll(PDO::FETCH_ASSOC);
+            Log::write("CONSULTA EXITOSA", "INFO");
+            return $result;
+        }catch(PDOException $e){
+            Log::write("dao\depot\DaoDepotImpl", "ERROR");
+			Log::write("ARCHIVO: " . $e->getFile() . " | lINEA DE ERROR: " . $e->getLine() . " | MENSAJE" . $e->getMessage(), "ERROR");
+			return array();
+        }
     }
     public function getByID($id){
 
