38ed0d4d54e66c195f8bb8e0c840f872d2f9d6e8
arcelioM <arceliomonte15@gmail.com>
2022-11-12 14:42:29 -0500

    Cambios hechos en Template para DAO

--- dao/IDaoTemplate.php
+++ dao/IDaoTemplate.php
@@ -6,6 +6,6 @@ interface IDaoTemplate{
 
     public function getAll();
     public function getByID($id);
-    public function save($entidad):int;
-    public function update($entidad):int;
+    public function save($entidad): ?int;
+    public function update($entidad): ?int;
 }
