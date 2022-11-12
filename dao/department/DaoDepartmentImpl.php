55d55a4543d3521978eba2ea3f473f8ca0132abc
arcelioM <arceliomonte15@gmail.com>
2022-11-12 14:44:01 -0500

    Cambios hechos en DAO para Deparment

--- dao/department/DaoDepartmentImpl.php
+++ dao/department/DaoDepartmentImpl.php
@@ -7,6 +7,7 @@ use PDOException;
 use util\Log;
 use PDO;
 use dao\department\IDaoDepartment;
+use model\Deparment;
 
 class DaoDepartmentImpl implements IDaoDepartment{
 
@@ -52,10 +53,96 @@ class DaoDepartmentImpl implements IDaoDepartment{
             return array();
         }
     }
-    public function save($entidad):int{
-        return 0;
-    }
-    public function update($entidad):int{
-        return 0;
+   
+    public function save($entidad): ?int{
+
+        try{
+            Log::write("INICIANDO GUARDADO DE DATOS | ".basename(__FILE__), "INSERT");
+            $query = "INSERT INTO deparment (name,idStatus) VALUES(?,?)";
+            $args = array(
+                $entidad->name,
+                $entidad->status->idStatus,
+                $entidad->idDepartment
+            );
+
+            $execute = $this->connection->getConnection()->prepare($query);
+
+            if($execute->execute($args)){
+                Log::write("DATOS GUARDADOS","INFO");
+                $idNewRow = $this->connection->getConnection()->lastInsertId();
+                return $idNewRow;
+            }else{
+                Log::write("FALLO DE INSERCION", "ERROR");
+            }
+
+            Log::write("OPERACION DE INSERCION TERMINADA","INFO");
+
+            return 0;
+            
+        }catch(PDOException $e){
+            Log::write("dao\status\DaoStatusImpl", "ERROR");
+			Log::write("ARCHIVO: " . $e->getFile() . " | lINEA DE ERROR: " . $e->getLine() . " | MENSAJE" . $e->getMessage(), "ERROR");
+			return 0;
+        }
+	}
+
+	public function update($entidad): ?int{
+		
+        try{
+
+            Log::write("ACTUALIZACION DE DATOS | ".basename(__FILE__),"UPDATE");
+            $query = "UPDATE deparment SET name = ?,idStatus=? WHERE ID_DEPARTMENT = ?";
+            $args = array(
+                $entidad->name,
+                $entidad->status->idStatus,
+                $entidad->idDepartment
+            );
+            $execute = $this->connection->getConnection()->prepare($query);
+            $rowAffected = $execute->execute($args);
+
+            if($rowAffected){
+                Log::write("REGISTRO ACTUALIZADO CORRECTAMENTE","INFO");
+                return 1;
+            }else{
+                Log::write("FALLO DE ACTUALIZACION", "ERROR");
+            }
+
+            Log::write("FIN DE OPERACION DE ACTUALIZACION","INFO");
+            return 0;
+
+        }catch(PDOException $e){
+            Log::write("dao\status\DaoStatusImpl", "ERROR");
+			Log::write("ARCHIVO: " . $e->getFile() . " | lINEA DE ERROR: " . $e->getLine() . " | MENSAJE" . $e->getMessage(), "ERROR");
+			return 0;
+        }
+	}
+
+    public function changeStatus(Deparment $entidad): ?int{
+        try{
+
+            Log::write("ACTUALIZACION STATUS DE DATOS | ".basename(__FILE__),"UPDATE");
+            $query = "UPDATE deparment SET idStatus=? WHERE ID_DEPARTMENT = ?";
+            $args = array(
+                $entidad->status->idStatus,
+                $entidad->idDepartment
+            );
+            $execute = $this->connection->getConnection()->prepare($query);
+            $rowAffected = $execute->execute($args);
+
+            if($rowAffected){
+                Log::write("REGISTRO ACTUALIZADO CORRECTAMENTE","INFO");
+                return 1;
+            }else{
+                Log::write("FALLO DE ACTUALIZACION", "ERROR");
+            }
+
+            Log::write("FIN DE OPERACION DE ACTUALIZACION","INFO");
+            return 0;
+
+        }catch(PDOException $e){
+            Log::write("dao\status\DaoStatusImpl", "ERROR");
+			Log::write("ARCHIVO: " . $e->getFile() . " | lINEA DE ERROR: " . $e->getLine() . " | MENSAJE" . $e->getMessage(), "ERROR");
+			return 0;
+        }
     }
 }
