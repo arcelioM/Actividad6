fa80e2ba7fbedfa862ad39fea3d46781e08f2ff0
arcelioM <arceliomonte15@gmail.com>
2022-11-12 00:46:46 -0500

    Objeto Entidad para Depot agregado

---
+++ model/Depot.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace model;
+
+class Depot{
+    private int $idDepot;
+
+    private String $branchName;
+
+    private String $location;
+
+    private Status $Status;
+
+    public function __construct(int $idDepot,String $branchName,String $location, Status $Status){
+        $this->idDepot = $idDepot;
+        $this->branchName = $branchName;
+        $this->location = $location;
+        $this->Status = $Status;
+    }
+
+    public function __get($name){
+        if(property_exists($this,$name)){
+           return $this->$name;
+        }
+     }
+ 
+     public function __set($name,$value){
+        if(property_exists($this,$name)){
+           return $this->$name=$value;
+        }
+     }
+
+    
+}
