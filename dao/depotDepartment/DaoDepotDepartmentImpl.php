1a5bc1347526c4abbf967723c5e5eeb27865e342
arcelioM <arceliomonte15@gmail.com>
2022-11-12 16:01:54 -0500

    Funcionalidad CRUD para cambio de estado para DepotDepartment

--- dao/depotDepartment/DaoDepotDepartmentImpl.php
+++ dao/depotDepartment/DaoDepotDepartmentImpl.php
@@ -4,6 +4,7 @@ namespace dao\depotDepartment;
 
 use dao\depotDepartment\IDaoDepotDepartment;
 use dao\connection\IConnection;
+use model\DepotDepartment;
 use PDOException;
 use util\Log;
 use PDO;
@@ -53,9 +54,100 @@ class DaoDepotDepartmentImpl implements IDaoDepotDepartment{
         }
     }
     public function save($entidad): ?int{
-        return 0;
+        
+        try{
+            Log::write("INICIO DE INSERCION","INSERT");
+            $query = "CALL sp_insert_depot_department(@result,?,?,?,?)";
+            $execute = $this->connection->getConnection()->prepare($query);
+
+            $execute->bindParam(1,$entidad->depot->idDepot,PDO::PARAM_INT,6);
+            $execute->bindParam(2,$entidad->deparment->idDepartment,PDO::PARAM_INT,6);
+            $execute->bindParam(3,$entidad->maxCapacity,PDO::PARAM_INT,6);
+            $execute->bindParam(4,$entidad->status->idStatus,PDO::PARAM_INT,6);
+            
+            $execute->execute();
+            $execute->closeCursor();
+
+            $result=$this->connection->getConnection()->query("SELECT @result as result")->fetchAll(PDO::FETCH_ASSOC);
+            $result=$result[0]["result"];
+
+
+            if($result==1){
+                Log::write("INSERCION EXITOSA","INFO");
+            }else{
+                Log::write("INSERCION HA FRACASADO","INFO");
+            }
+
+            
+            return $result;
+
+        }catch(PDOException $e){
+            Log::write("dao\depotDepartment\DaoDepotDepartmentImpl", "ERROR");
+            Log::write("ARCHIVO: " . $e->getFile() . " | lINEA DE ERROR: " . $e->getLine() . " | MENSAJE" . $e->getMessage(), "ERROR");
+            return null;
+        }
     }
     public function update($entidad): ?int{
-        return 0;
+        
+
+        try{
+
+            Log::write("INICIO DE ACTUALIZACION","UPDATE");
+            $query = "UPDATE depotdepartment SET idDepot=?, idDepartment=?,idStatus=?,maxCapacity=? WHERE ID_DEPOT_DEPARTMENT=?";
+            $args=array(
+                $entidad->depot->idDepot,
+                $entidad->deparment->idDepartment,
+                $entidad->status->idStatus,
+                $entidad->maxCapacity,
+                $entidad->idDepotDepartment
+            );
+
+            $execute = $this->connection->getConnection()->prepare($query);
+
+            
+            $resultRowAffect=$execute->execute($args);
+            if($resultRowAffect){
+                Log::write("REGISTRO ACTUALIZADO CORRECTAMENTE","INFO");
+                return 1;
+            }else{
+                Log::write("FALLO DE ACTUALIZACION", "ERROR");
+                return 0;
+            }
+
+        }catch(PDOException $e){
+            Log::write("dao\depotDepartment\DaoDepotDepartmentImpl", "ERROR");
+            Log::write("ARCHIVO: " . $e->getFile() . " | lINEA DE ERROR: " . $e->getLine() . " | MENSAJE" . $e->getMessage(), "ERROR");
+            return null;
+        }
+    }
+
+    public function changeStatus(DepotDepartment $entidad): ?int{
+
+        try{
+
+            Log::write("INICIO DE ACTUALIZACION STATUS","UPDATE");
+            $query = "UPDATE depotdepartment SET idStatus=? WHERE ID_DEPOT_DEPARTMENT=?";
+            $args=array(
+                $entidad->status->idStatus,
+                $entidad->idDepotDepartment
+            );
+
+            $execute = $this->connection->getConnection()->prepare($query);
+
+            
+            $resultRowAffect=$execute->execute($args);
+            if($resultRowAffect){
+                Log::write("REGISTRO ACTUALIZADO CORRECTAMENTE","INFO");
+                return 1;
+            }else{
+                Log::write("FALLO DE ACTUALIZACION", "ERROR");
+                return 0;
+            }
+
+        }catch(PDOException $e){
+            Log::write("dao\depotDepartment\DaoDepotDepartmentImpl", "ERROR");
+            Log::write("ARCHIVO: " . $e->getFile() . " | lINEA DE ERROR: " . $e->getLine() . " | MENSAJE" . $e->getMessage(), "ERROR");
+            return null;
+        }
     }
 }
--- dao/depotDepartment/IDaoDepotDepartment.php
+++ dao/depotDepartment/IDaoDepotDepartment.php
@@ -2,6 +2,9 @@
 
 namespace dao\depotDepartment;
 use dao\IDaoTemplate;
+use model\DepotDepartment;
 
 interface IDaoDepotDepartment extends IDaoTemplate{
+
+    public function changeStatus(DepotDepartment $depotDepartment): ?int;
 }
