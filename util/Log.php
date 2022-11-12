881d5d792f7cef331e66eab0364792fd77a53d52
arcelioM <arceliomonte15@gmail.com>
2022-11-12 14:53:50 -0500

    Modificaciones  a Archivo de Log

--- util/Log.php
+++ util/Log.php
@@ -14,7 +14,7 @@ class Log{
     public static function  write(String $message, String $mode){
         $log=new Log();
         $date = new DateTime(); 
-        fputs($log->fileLog,"[".$mode."] [".$date->format('Y-m-d H-i-s')."] | ".$message." \n");
+        fputs($log->fileLog,"[".$mode."] [".$date->format('Y-m-d | H:i:s')."] | ".$message." \n");
         fclose($log->fileLog);
     }
 }
