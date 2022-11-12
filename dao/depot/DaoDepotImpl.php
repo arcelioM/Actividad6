37d80d7e161c17265d25773f143a80551c73da93
arcelioM <arceliomonte15@gmail.com>
2022-11-12 14:45:02 -0500

    Modifiacion a Interface para Depot

--- dao/depot/DaoDepotImpl.php
+++ dao/depot/DaoDepotImpl.php
@@ -3,6 +3,7 @@
 namespace dao\depot;
 
 use dao\connection\IConnection;
+use model\Depot;
 use PDOException;
 use util\Log;
 use PDO;
@@ -52,12 +53,96 @@ class DaoDepotImpl implements IDaoDepot
             return array();
         }
     }
-    public function save($entidad): int
-    {
-        return 0;
-    }
-    public function update($entidad): int
-    {
-        return 0;
+    public function save($entidad): ?int{
+
+        try{
+            Log::write("INICIANDO GUARDADO DE DATOS | ".basename(__FILE__), "INSERT");
+            $query = "INSERT INTO depot (branchName,location,idStatus) VALUES(?,?,?)";
+            $args = array(
+                $entidad->branchName,
+                $entidad->location,
+                $entidad->status->idStatus
+            );
+            $execute = $this->connection->getConnection()->prepare($query);
+
+            if($execute->execute($args)){
+                Log::write("DATOS GUARDADOS","INFO");
+                $idNewRow = $this->connection->getConnection()->lastInsertId();
+                return $idNewRow;
+            }else{
+                Log::write("FALLO DE INSERCION", "ERROR");
+            }
+
+            Log::write("OPERACION DE INSERCION TERMINADA","INFO");
+
+            return 0;
+            
+        }catch(PDOException $e){
+            Log::write("dao\depot\DaoDepotImpl", "ERROR");
+			Log::write("ARCHIVO: " . $e->getFile() . " | lINEA DE ERROR: " . $e->getLine() . " | MENSAJE" . $e->getMessage(), "ERROR");
+			return 0;
+        }
+	}
+
+	public function update($entidad): ?int{
+		
+        try{
+
+            Log::write("ACTUALIZACION DE DATOS | ".basename(__FILE__),"UPDATE");
+            $query = "UPDATE depot SET branchName=?,location=?,idStatus=? WHERE ID_DEPOT = ?";
+            $args = array(
+                $entidad->branchName,
+                $entidad->location,
+                $entidad->status->idStatus,
+                $entidad->idDepot
+            );
+            $execute = $this->connection->getConnection()->prepare($query);
+            $rowAffected = $execute->execute($args);
+
+            if($rowAffected){
+                Log::write("REGISTRO ACTUALIZADO CORRECTAMENTE","INFO");
+                return 1;
+            }else{
+                Log::write("FALLO DE ACTUALIZACION", "ERROR");
+            }
+
+            Log::write("FIN DE OPERACION DE ACTUALIZACION","INFO");
+            return 0;
+
+        }catch(PDOException $e){
+            Log::write("dao\depot\DaoDepotImpl", "ERROR");
+			Log::write("ARCHIVO: " . $e->getFile() . " | lINEA DE ERROR: " . $e->getLine() . " | MENSAJE" . $e->getMessage(), "ERROR");
+			return 0;
+        }
+	}
+
+    public function changeStatus(Depot $entidad): ?int{
+
+        try{
+
+            Log::write("ACTUALIZACION STATUS DE DATOS | ".basename(__FILE__),"UPDATE");
+            $query = "UPDATE depot SET idStatus=? WHERE ID_DEPOT = ?";
+            $args = array(
+                $entidad->status->idStatus,
+                $entidad->idDepot
+            );
+            $execute = $this->connection->getConnection()->prepare($query);
+            $rowAffected = $execute->execute($args);
+
+            if($rowAffected){
+                Log::write("REGISTRO ACTUALIZADO CORRECTAMENTE","INFO");
+                return 1;
+            }else{
+                Log::write("FALLO DE ACTUALIZACION", "ERROR");
+            }
+
+            Log::write("FIN DE OPERACION DE ACTUALIZACION","INFO");
+            return 0;
+
+        }catch(PDOException $e){
+            Log::write("dao\depot\DaoDepotImpl", "ERROR");
+			Log::write("ARCHIVO: " . $e->getFile() . " | lINEA DE ERROR: " . $e->getLine() . " | MENSAJE" . $e->getMessage(), "ERROR");
+			return 0;
+        }
     }
 }
