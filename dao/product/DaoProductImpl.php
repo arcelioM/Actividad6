5dc47617e7252c292478497f91ee5fb838386f8e
arcelioM <arceliomonte15@gmail.com>
2022-11-12 14:51:45 -0500

    Modificaciones para DAO de Produt

--- dao/product/DaoProductImpl.php
+++ dao/product/DaoProductImpl.php
@@ -54,13 +54,71 @@ class DaoProductImpl implements IDaoProduct
 		}
 	}
 
-	public function save($entidad): int
-	{
-		return 0;
+	public function save($entidad): ?int{
+
+        try{
+            Log::write("INICIANDO GUARDADO DE DATOS | ".basename(__FILE__), "INSERT");
+            $query = "INSERT INTO product (name,idTypeProduct,price,experationDate,idStatus) VALUES(?,?,?,?,?)";
+
+            $args = array(
+				$entidad->name,
+				$entidad->typeProduct->idTypeProduct,
+				$entidad->price,
+				$entidad->experationDate,
+				$entidad->status->idStatus
+			);
+            $execute = $this->connection->getConnection()->prepare($query);
+
+            if($execute->execute($args)){
+                Log::write("DATOS GUARDADOS","INFO");
+                $idNewRow = $this->connection->getConnection()->lastInsertId();
+                return $idNewRow;
+            }else{
+                Log::write("FALLO DE INSERCION", "ERROR");
+            }
+
+            Log::write("OPERACION DE INSERCION TERMINADA","INFO");
+
+            return 0;
+            
+        }catch(PDOException $e){
+            Log::write("dao\status\DaoStatusImpl", "ERROR");
+			Log::write("ARCHIVO: " . $e->getFile() . " | lINEA DE ERROR: " . $e->getLine() . " | MENSAJE" . $e->getMessage(), "ERROR");
+			return 0;
+        }
 	}
 
-	public function update($entidad): int
-	{
-		return 0;
+	public function update($entidad): ?int{
+		
+        try{
+
+            Log::write("ACTUALIZACION DE DATOS | ".basename(__FILE__),"UPDATE");
+            $query = "UPDATE product SET name = ?,idTypeProduct=?,price=?,experationDate=?,idStatus=? WHERE ID_PRODUCT = ?";
+            $args = array(
+				$entidad->name,
+				$entidad->typeProduct->idTypeProduct,
+				$entidad->price,
+				$entidad->experationDate,
+				$entidad->status->idStatus,
+				$entidad->idProduct
+			);
+            $execute = $this->connection->getConnection()->prepare($query);
+            $rowAffected = $execute->execute($args);
+
+            if($rowAffected){
+                Log::write("REGISTRO ACTUALIZADO CORRECTAMENTE","INFO");
+                return 1;
+            }else{
+                Log::write("FALLO DE ACTUALIZACION", "ERROR");
+            }
+
+            Log::write("FIN DE OPERACION DE ACTUALIZACION","INFO");
+            return 0;
+
+        }catch(PDOException $e){
+            Log::write("dao\status\DaoStatusImpl", "ERROR");
+			Log::write("ARCHIVO: " . $e->getFile() . " | lINEA DE ERROR: " . $e->getLine() . " | MENSAJE" . $e->getMessage(), "ERROR");
+			return 0;
+        }
 	}
 }
