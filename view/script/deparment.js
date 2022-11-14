f00d46e549aa333a5e070e45cef4b0b9d26c17a2
arcelioM <arceliomonte15@gmail.com>
2022-11-14 01:51:19 -0500

    Script de controlador para vista Department agregado

---
+++ view/script/deparment.js
@@ -0,0 +1,42 @@
+$(function () {
+    
+
+    $.soap({
+        url: "http://localhost/workspace-php/poo/Actividad6/controller/department/ControllerDeparment.php?wsdl",
+        method: 'getAll',
+        
+        data: {
+        },
+        
+        success: getData,
+        error: function (SOAPResponse) {
+            $("#result").html(SOAPResponse);
+        }
+    });
+
+
+    function getData(response) {
+        $("#tBody").html("");
+        data = response.toJSON().Body.getAllResponse.return;
+        data = JSON.parse(data);
+        data = data["values"]
+        let rowData ="";
+        for(let i=0; i<data.length;i++){
+            showData(data[i],rowData);
+        }
+
+        console.log(data[1]["idProduct"]);
+
+    }
+
+    function showData(data) {
+        
+        rowData ="<tr>";
+        rowData += "<td>"+data.ID_DEPARTMENT+"</td>";
+        rowData +="<td>"+data.name+"</td>";
+        rowData +="<td>"+data.idStatus+"</td>";
+        rowData +="<td>"+data.creationDate+"</td>";
+        rowData +="</tr>";
+        $("#tBody").append(rowData);
+    }
+});
